45f0d0cf607868bc5d18ecb6b4037429be7ed3b1
Tollan <tollan.renner@gmail.com>
2020-02-09 17:27:53 -0600

    local updates

---
+++ Algorithm.CSharp/VXXSurfer.cs
@@ -0,0 +1,115 @@
+﻿using QuantConnect.Indicators;
+using QuantConnect.Orders.Fees;
+using QuantConnect.Data.Market;
+
+namespace QuantConnect.Algorithm.CSharp
+{
+    public class VXXSurfer : QCAlgorithm
+    {
+        //Define required variables:
+        private const string symbol = "VXX";
+        private const decimal shortSL = 1.03m;
+        //    private const decimal longSL = 0.90m;
+        //    private const decimal drawdown = 0.96m; // 0.97 exp .57
+
+        private const int endHour = 11;
+
+        private decimal holdings = 0;
+        private decimal price = 0;
+        private decimal dayOpen = 0;
+        private decimal stopLoss = 0;
+        //    private decimal close1 = 0;
+
+        //Set up the indicator Classes:
+        private SimpleMovingAverage close0;
+        private RateOfChange roc1;
+        private RateOfChange roc2;
+        private RateOfChange roc3;
+        private RateOfChange roc4;
+        private AverageTrueRange atr1;
+        private RelativeStrengthIndex rsi2;
+
+        private ExponentialMovingAverage emaShort;
+
+        //Initialize the data and resolution you require for your strategy:
+        public override void Initialize()
+        {
+            SetStartDate(2017, 1, 3);  //SetStartDate(2009, 1, 30);
+            SetEndDate(2018, 2, 15);   //SetEndDate(2015, 11, 30);
+            SetCash(10000);
+            AddSecurity(SecurityType.Equity, symbol, Resolution.Minute); // Resolution.Daily); 
+
+            close0 = SMA(symbol, 1, Resolution.Daily);
+
+            roc1 = ROC(symbol, 1, Resolution.Daily);
+            roc2 = ROC(symbol, 2, Resolution.Daily);
+            roc3 = ROC(symbol, 3, Resolution.Daily);
+            roc4 = ROC(symbol, 10, Resolution.Daily);
+            atr1 = ATR(symbol, 1, MovingAverageType.Simple, Resolution.Daily);
+            rsi2 = RSI(symbol, 2, MovingAverageType.Simple, Resolution.Daily);
+            emaShort = EMA(symbol, 10, Resolution.Daily);
+
+            // Set commissions to $1
+            Securities[symbol].FeeModel = new ConstantFeeModel(1);
+
+        }
+
+        public void OnData(TradeBars data)
+        {
+            // wait for slowest indicator to fully initialize
+            if (!emaShort.IsReady) return;
+
+            TradeBar b = null;
+            data.TryGetValue(symbol, out b);
+            price = b.Close;
+            holdings = Portfolio[symbol].Quantity;
+
+            // EXIT
+            if (b.Time.Minute == 45
+                && b.Time.Hour == endHour
+                && holdings > 0
+                ) // exit long positions 1-2 hours after market open
+                Liquidate();
+
+            if (holdings < 0
+                && (roc2 > 0
+                || price > stopLoss)
+                ) // exit short positions with trailing stop or two-day uptrend
+                Liquidate();
+
+            // On market open
+            if (b.Time.Hour == 9 && b.Time.Minute == 30) dayOpen = b.Open;
+
+            // ENTRY
+            if (b.Time.Hour == 9 && b.Time.Minute == 30
+                // SAFETY -- only enter a new position if in cash
+                //	&& Portfolio.Invested == false
+                )
+            {
+                // check for long entry
+                if ((roc1 > 0   // last trading day closed higher than the previous trading day
+                    && roc2 > 0
+                    && roc3 > 0)
+                    && roc4 > 0
+                    && dayOpen < close0 * 1.00m // today opened below last trading day's close
+                    && price > emaShort
+                    )
+                {
+                    SetHoldings(symbol, 1.0m, false, "open");
+                    //	stopLoss = price*longSL;
+                }
+                // check for short entry
+                if (roc2 < 0)   // two-day downtrend   
+                {
+                    SetHoldings(symbol, -1.0m, false, "open");
+                    stopLoss = price * shortSL;
+                }
+            } // if
+
+            // update trailing stop loss
+            if (holdings < 0 && price * shortSL < stopLoss)
+                stopLoss = price * shortSL;
+
+        } // OnData
+    } // QCAlgo
+}
---
+++ Algorithm.Python/fomc_algo.py
@@ -0,0 +1,99 @@
+﻿from clr import AddReference
+AddReference("System")
+AddReference("System.Core")
+AddReference("QuantConnect.Algorithm")
+AddReference("QuantConnect.Common")
+#AddReference("QuantConnect.Indicators")
+
+from System import *
+from System.Collections.Generic import List
+# from System.Drawing import Color
+from QuantConnect import *
+from QuantConnect.Algorithm import *
+#from QuantConnect.Indicators import *
+from datetime import *
+from decimal import *
+import pandas as pd
+from pandas.tseries.offsets import *
+
+# Cieslak, Morse & Vissing-Jorgensen (2014)
+# http://pages.stern.nyu.edu/~adamodar/New_Home_Page/datafile/histretSP.html
+class FOMCAlgorithm(QCAlgorithm):
+    
+    def Initialize(self):
+        self.trade_symbol = "SPY"
+        self.AddEquity(self.trade_symbol)
+        self.SetCash(100000)
+        self.SetCalendar(date(1998,1,1), date(2018,1,1))
+        self.SetBenchmark(self.trade_symbol)
+
+        self.stopLoss = 0
+        
+    def SetCalendar(self, start_date, end_date):
+        self.SetStartDate(start_date)
+        self.SetEndDate(end_date)
+        url = "https://www.dropbox.com/s/njjthytp1q0oqd7/fomc_calendar.csv?dl=1"
+        self.fomc_calendar = pd.read_csv(url, index_col=0, parse_dates=[0], infer_datetime_format=True)
+    
+    def OnData(self, data):
+        # self.hold_fomc_week() 
+        # self.on_fomc_week()
+        self.hybrid_fomc_week2() # best
+        pass
+    
+    def hold_fomc_week(self):
+        if not (self.Time.hour <= 9 and self.Time.minute <= 31): return
+        fomc_week = self.fomc_calendar.loc[self.Time.date()]['fomc_week']
+        is_even_fomc_week = fomc_week % 2 == 0
+        if self.Portfolio[self.trade_symbol].Invested:
+            if not is_even_fomc_week:
+                self.Liquidate()
+        elif is_even_fomc_week:
+            self.SetHoldings(self.trade_symbol, 1)
+    
+    def on_fomc_week(self):
+        if not (self.Time.hour >= 15 and self.Time.minute >= 59): return
+        next_fomc_week = self.fomc_calendar.loc[self.Time.date() + BDay(1)]['fomc_week']
+        is_even_fomc_week = next_fomc_week % 2 == 0
+        if self.Portfolio[self.trade_symbol].Invested:
+            if not is_even_fomc_week:
+                self.Liquidate()
+                # tag = "Exit-Week: {0} Day: {1}".format(self.fomc_calendar.loc[self.Time.date()]['fomc_week'], self.fomc_calendar.loc[self.Time.date()]['fomc_day'])
+                # self.MarketOrder(self.trade_symbol, -self.Portfolio[self.trade_symbol].Quantity, False, tag)
+        elif is_even_fomc_week:
+            self.SetHoldings(self.trade_symbol, 1)
+            # tag = "Entry-Week: {0} Day: {1}".format(self.fomc_calendar.loc[self.Time.date()]['fomc_week'], self.fomc_calendar.loc[self.Time.date()]['fomc_day'])
+            # max_shares = self.Portfolio.TotalPortfolioValue // self.Securities[self.trade_symbol].Price
+            # self.MarketOrder(self.trade_symbol, max_shares, False, tag)
+    
+    def hybrid_fomc_week(self):
+        if (self.Time.hour >= 15 and self.Time.minute >= 59 and
+            self.Portfolio[self.trade_symbol].Invested):
+            next_fomc_week = self.fomc_calendar.loc[self.Time.date() + BDay(1)]['fomc_week']
+            is_next_week_even = next_fomc_week % 2 == 0
+            if not is_next_week_even:
+                self.Liquidate()
+        if (self.Time.hour <= 9 and self.Time.minute <= 31 and
+            not self.Portfolio[self.trade_symbol].Invested):
+            fomc_week = self.fomc_calendar.loc[self.Time.date()]['fomc_week']
+            is_even_fomc_week = fomc_week % 2 == 0
+            if is_even_fomc_week:
+                self.SetHoldings(self.trade_symbol, 1)
+    
+    def hybrid_fomc_week2(self):
+        if (self.Time.hour <= 9 and self.Time.minute <= 31 and
+            self.Portfolio[self.trade_symbol].Invested):
+            fomc_week = self.fomc_calendar.loc[self.Time.date()]['fomc_week']
+            is_even_fomc_week = fomc_week % 2 == 0
+            if not is_even_fomc_week: # or self.Securities[self.trade_symbol].Price < self.stopLoss:                
+                self.Liquidate()
+        if (self.Time.hour >= 15 and self.Time.minute >= 59 and
+            not self.Portfolio[self.trade_symbol].Invested):
+            next_fomc_week = self.fomc_calendar.loc[self.Time.date() + BDay(1)]['fomc_week']
+            is_next_week_even = next_fomc_week % 2 == 0
+            if is_next_week_even:
+                self.SetHoldings(self.trade_symbol, 1)
+    
+    def OnEndOfDay(self):
+        self.stopLoss = self.Securities[self.trade_symbol].Price * 0.98
+        #self.Log(str(self.Securities[self.trade_symbol].Price))
